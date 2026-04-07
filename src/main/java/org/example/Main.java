e07de963f6a7b7f1f24d0031cda97b5e3ee87951
Judith-olmand <judith.olmand@educa.jcyl.es>
2026-04-07 09:48:08 +0200

    acabado

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,22 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+import java.sql.*;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
+public class Main {
+    public static void main(String[] args) {
+        try (Connection conn = DriverManager.getConnection(
+                DBConfig.getUrl(),
+                DBConfig.getUser(),
+                DBConfig.getPassword()); Statement statement = conn.createStatement()){
+            System.out.println("Conexión establecida con Oracle.");
+            String sql = "DELETE FROM empleado WHERE ID = ?";
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setInt(1, 10);
+            int filasEliminadas = ps.executeUpdate();
+            System.out.println("Empleado eliminado con éxito" + filasEliminadas);
+
+        } catch (SQLException e) {
+            System.out.println("Error al conectar: " + e.getMessage());
         }
     }
 }
